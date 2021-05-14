3cb851d8c64e7d13f878e452f54fcf6a1722c030
Madmandy <63957307+Madmandy@users.noreply.github.com>
2021-05-14 10:58:56 +0530

    Create index.php

---
+++ script/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>script</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+<body>
+        
+        <?php
+        if(isset($_POST['submit'])){
+
+            $title = $_POST['title'];
+            $img  = $_POST['img'];
+            $keyword = $_POST['keyword'];
+            $discription  = $_POST['discription'];
+            $link  = $_POST['link'];
+        }
+        ?>
+
+
+<form action="" method="POST">
+
+<div>
+<input name="title" type="text" class="form-control" placeholder="title" required="required">
+</div>
+
+<div>
+<input name="img" type="text" class="form-control" placeholder="img" required="required">
+</div>
+
+<div>
+<input name="keyword" type="text" class="form-control" placeholder="keyword" required="required">
+</div>
+
+<div>
+<input name="discription" type="text" class="form-control" placeholder="discription" required="required">
+</div>
+
+<div>
+<input name="link" type="text" class="form-control" placeholder="link" required="required">
+</div>
+
+<div class="form-group">
+<button name="submit" type="submit" class="btn btn-primary btn-block">Submit</button>
+</div>
+
+</form>
+
+<div>
+<textarea name="" id="" cols="30" rows="10">
+
+<?php echo $title?>
+<?php echo $img ?>
+<?php echo $keyword ?> 
+<?php echo $discription ?> 
+<?php echo $link ?>
+        
+
+</textarea>
+
+</div>
+
+</body>
+
+</html>
